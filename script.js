daaaec42d920489dcf02fc50b47d2b48375b6ab4
Stanislaw Dymny <dymnystan@gmail.com>
2023-12-17 01:35:31 +0100

    videos & audio files coordinations fixed

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 'use strict'
 
-let videoEl = document.querySelector('video')
+let currentPlayer = document.querySelector('video')
+
+const audioPlayer = document.querySelector("audio")
+const videoPlayer = document.querySelector("video")
+
 const showCurrVidEl = document.querySelector('.current-video')
 const playBarEl = document.querySelector('.video-bar')
 const videoHider = document.querySelector('.video-shadowing')
 
 let currVid = 0
-const vidURLs = {
-	getFileUrl:index => "./files/" + vidURLs.files[index],
-	files:[
-	'Wspolczesna_kolenda(edit).mp4',
-	'Ave Verum by Albinoni (Adagio in G Minor).mp4',
-	'Bóg jest Miłością-(480p).mp4',
-	'Przybieżeli do Betlejem.mp4']
+
+const vid = {
+	URL:[
+		"1. Słowo.mp3","2.Prawdziwa światłość.mp3","3. A słowo.mp3","4. Rozmowa.mp3","5. Wzgardzony i odepchnięty 1.mp3","5. Wzgardzony i odepchnięty 2.mp3","6. lecz on się obarczył 1.mp3","6. lecz on się obarczył 2.mp3","8. jak baranek 1.mp3","8. jak baranek 2.mp3","9. Umiweram w twoim ubraniu.mp3","Ave Verum by Albinoni (Adagio in G Minor).mp4","Bóg jest Miłością-(480p).mp4","Przybieżeli do Betlejem.mp4","Wspolczesna_kolenda(edit).mp4","nie zapomnij o mnie 1.mp3","nie zapomnij o mnie 2.mp3","nie zapomnij o mnie 3.mp3","nie zapomnij o mnie 4.mp3"
+	],
+	audioExtensions:['mp3', 'ogg', 'wav', 'aac', 'flac'],
+	getFileUrl:index => "./files/" + vid.URL[index],
+	isAudio:link => vid.audioExtensions.includes(link.slice(link.lastIndexOf('.') + 1)),
 }
 
-var audioExtensions = ['mp3', 'ogg', 'wav', 'aac', 'flac']
+let trackIsAudio = vid.isAudio(vid.URL[0]);
+if(trackIsAudio) {
+	currentPlayer = document.querySelector("audio")
+}
+else {
+	currentPlayer = document.querySelector("video")
+}
 
-videoEl.src = vidURLs.getFileUrl(0)
-showCurrVidEl.textContent = vidURLs.getFileUrl(0)
+currentPlayer.src = vid.getFileUrl(0)
+showCurrVidEl.textContent = vid.URL[0]
 
-videoEl.currentTime = 0.1
+currentPlayer.currentTime = 0.1
 
 function playPause() {
-	if (videoEl.paused) {
-		videoEl.play()
+	if (currentPlayer.paused) {
+		currentPlayer.play()
 	} else {
-		videoEl.pause()
+		currentPlayer.pause()
 	}
 }
 document.addEventListener('keydown', e => {
@@ -35,22 +46,22 @@ document.addEventListener('keydown', e => {
 			playPause()
 			break
 		case 'KeyR':
-			videoEl.currentTime = 0
+			currentPlayer.currentTime = 0
 			hideVid()
-			videoEl.pause()
+			currentPlayer.pause()
 			break
 		case 'ArrowLeft':
-			if (videoEl.currentTime > 5) {
-				videoEl.currentTime -= 5
+			if (currentPlayer.currentTime > 5) {
+				currentPlayer.currentTime -= 5
 			} else {
-				videoEl.currentTime = 0.1
+				currentPlayer.currentTime = 0.1
 			}
 			break
 		case 'ArrowRight':
-			if (videoEl.currentTime + 5 < videoEl.duration) {
-				videoEl.currentTime += 5
+			if (currentPlayer.currentTime + 5 < currentPlayer.duration) {
+				currentPlayer.currentTime += 5
 			} else {
-				videoEl.currentTime = videoEl.duration
+				currentPlayer.currentTime = currentPlayer.duration
 			}
 			break
 		case 'KeyS':
@@ -66,18 +77,15 @@ document.addEventListener('keydown', e => {
 			}
 			break
 		case 'Period':
-			if (vidURLs.files.length - 1 !== currVid) {
+			if (vid.URL.length - 1 !== currVid) {
 				hideVid()
 				changeVid(++currVid)
 			}
 			break
 	}
 })
-function updateProgressBar() {
-	const progress = (videoEl.currentTime / videoEl.duration) * 100
-	playBarEl.value = progress
-}
-const isAudio = link => audioExtensions.includes(link.slice(link.lastIndexOf('.') + 1))
+const updateProgressBar = () =>	playBarEl.value = (currentPlayer.currentTime / currentPlayer.duration) * 100
+
 
 const hideCoursor = () => {
   if (document.body.style.cursor === 'none') {
@@ -98,33 +106,17 @@ const dataVisibility = () => {
 }
 
 async function changeVid(index) {
+	const vidURL = vid.getFileUrl(index)
 	await lowerSound()
-		const vidURL = vidURLs.getFileUrl(index)
-		if (isAudio(vidURL)) {
-			videoEl = document.querySelector('audio')
-		} else {
-			videoEl = document.querySelector('video')
-		}	
-		videoEl.src = vidURL
-		showCurrVidEl.textContent = vidURL
-		videoEl.currentTime = 0.1
-		playBarEl.max = videoEl.duration
+		updatePlayer(vidURL);
+		currentPlayer.src = vidURL
+		showCurrVidEl.textContent = vid.URL[index]
+		currentPlayer.currentTime = 0.1
+		playBarEl.max = currentPlayer.duration
+		console.log(vid.URL[index])
 		updateProgressBar()
 }
-videoEl.addEventListener('timeupdate', updateProgressBar)
 
-playBarEl.addEventListener('input', function () {
-	const seekTime = (playBarEl.value / 100) * videoEl.duration
-	videoEl.currentTime = seekTime
-})
-
-videoEl.addEventListener('ended', () => {
-	hideVid()
-})
-videoEl.addEventListener('play', () => {
-	videoEl.volume = 1
-	showVid()
-})
 const hideVid = () => {
 	videoHider.classList.add('shown')
 	videoHider.classList.remove('hidden')
@@ -133,6 +125,26 @@ const showVid = () => {
 	videoHider.classList.add('hidden')
 	videoHider.classList.remove('shown')
 }
+const updatePlayer = (vidLink) => {
+	if (vid.isAudio(vidLink)) {
+		currentPlayer = audioPlayer
+	} else {
+		currentPlayer = videoPlayer
+	}
+	currentPlayer.addEventListener('ended', () => {
+		hideVid()
+	})
+	currentPlayer.addEventListener('play', () => {
+		currentPlayer.volume = 1
+		showVid()
+	})
+	playBarEl.addEventListener('input', function () {
+		const seekTime = (playBarEl.value / 100) * currentPlayer.duration
+		currentPlayer.currentTime = seekTime
+	})
+	currentPlayer.addEventListener('timeupdate', updateProgressBar)
+}
+updatePlayer(vid.getFileUrl(0));
 const lowerSound = (lowerTo = 0) => {
   return new Promise (resolve => {
   let vol = 1000
@@ -142,8 +154,8 @@ const lowerSound = (lowerTo = 0) => {
       resolve()
 		}
 		vol -= 2
-		videoEl.volume = vol <= 0 ? 0 : Number('0.' + `${vol}`.padStart(3, '0'))
-		if (vol % 100 === 0) console.log(videoEl.volume)
+		currentPlayer.volume = vol <= 0 ? 0 : Number('0.' + `${vol}`.padStart(3, '0'))
+		if (vol % 100 === 0) console.log(currentPlayer.volume * 10)
 	}, 1)
   })
 }
