79b8d8e2d0af092cd6f85850688cda34cce9430f
Stanislaw Dymny <dymnystan@gmail.com>
2023-12-18 01:12:41 +0100

    bug fixed

--- script.js
+++ script.js
@@ -13,19 +13,18 @@ let currVid = 0
 
 const vid = {
 	URL:[
-		"1. Słowo.mp3","2.Prawdziwa światłość.mp3","3. A słowo.mp3","4. Rozmowa.mp3","5. Wzgardzony i odepchnięty 1.mp3","5. Wzgardzony i odepchnięty 2.mp3","6. lecz on się obarczył 1.mp3","6. lecz on się obarczył 2.mp3","8. jak baranek 1.mp3","8. jak baranek 2.mp3","9. Umiweram w twoim ubraniu.mp3","Ave Verum by Albinoni (Adagio in G Minor).mp4","Bóg jest Miłością-(480p).mp4","Przybieżeli do Betlejem.mp4","Wspolczesna_kolenda(edit).mp4","nie zapomnij o mnie 1.mp3","nie zapomnij o mnie 2.mp3","nie zapomnij o mnie 3.mp3","nie zapomnij o mnie 4.mp3"
+		"0. Wspolczesna_kolenda(edit).mp4","1 Słowo EDITED.mp3","2. Bóg jest Miłością-(480p).mp4","3. Ave Verum by Albinoni (Adagio in G Minor).mp4","4. Rozmowa EDIT.mp3","4. Wzgardzony i odepchnięty 1 EDIT.mp3","5. Nie zapomnij o mnie 2.mp3","Przybieżeli do Betlejem.mp4"
 	],
 	audioExtensions:['mp3', 'ogg', 'wav', 'aac', 'flac'],
 	getFileUrl:index => "./files/" + vid.URL[index],
 	isAudio:link => vid.audioExtensions.includes(link.slice(link.lastIndexOf('.') + 1)),
 }
 
-let trackIsAudio = vid.isAudio(vid.URL[0]);
-if(trackIsAudio) {
-	currentPlayer = document.querySelector("audio")
+if(vid.isAudio(vid.URL[0])) {
+	currentPlayer = audioPlayer
 }
 else {
-	currentPlayer = document.querySelector("video")
+	currentPlayer = videoPlayer
 }
 
 currentPlayer.src = vid.getFileUrl(0)
@@ -105,9 +104,9 @@ const dataVisibility = () => {
   }
 }
 
-async function changeVid(index) {
+async function changeVid(index,soundFading=true) {
 	const vidURL = vid.getFileUrl(index)
-	await lowerSound()
+	if (soundFading) await lowerSound()
 		updatePlayer(vidURL);
 		currentPlayer.src = vidURL
 		showCurrVidEl.textContent = vid.URL[index]
@@ -127,20 +126,27 @@ const showVid = () => {
 }
 const updatePlayer = (vidLink) => {
 	if (vid.isAudio(vidLink)) {
-		currentPlayer = audioPlayer
+	   currentPlayer.removeEventListener('ended', hideVid);
+	   currentPlayer.removeEventListener('play', showVid);
+	   currentPlayer = audioPlayer;
 	} else {
-		currentPlayer = videoPlayer
+	   currentPlayer.removeEventListener('ended', hideVid);
+	   currentPlayer.removeEventListener('play', showVid);
+	   currentPlayer = videoPlayer;
 	}
 	currentPlayer.addEventListener('ended', () => {
 		hideVid()
 	})
 	currentPlayer.addEventListener('play', () => {
 		currentPlayer.volume = 1
-		showVid()
+		if (!vid.isAudio(vid.URL[currVid])) showVid()
 	})
 	playBarEl.addEventListener('input', function () {
 		const seekTime = (playBarEl.value / 100) * currentPlayer.duration
 		currentPlayer.currentTime = seekTime
+	})
+	currentPlayer.addEventListener('ended',()=>{
+		changeVid(currVid === vid.URL.length - 1 ? currVid : ++currVid,false)
 	})
 	currentPlayer.addEventListener('timeupdate', updateProgressBar)
 }
